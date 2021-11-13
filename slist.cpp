ced955839d8cfd6513c32dac55f7e4a0127f1209
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-11-12 20:58:38 -0800

    Update slist.cpp

--- slist.cpp
+++ slist.cpp
@@ -17,16 +17,16 @@ struct student{
 int main(){
 
   cout << "Welcome to the Student List." << endl;
+  cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
+  cout << "ADD -> Creates a new entry for student" << endl;
+  cout << "PRINT -> Prints the current students stored" << endl;
+  cout << "DELETE -> Deletes entry based on student ID" << endl;
+  cout << "QUIT -> Exits the program" << endl;
   int loop = 0;
 
   while(loop == 0){
 
     cout << "What would you like to do?" << endl;
-    cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
-    cout << "ADD -> Creates a new entry for student" << endl;
-    cout << "PRINT -> Prints the current students stored" << endl;
-    cout << "DELETE -> Deletes entry based on student ID" << endl;
-    cout << "QUIT -> Exits the program" << endl;
     cout << "Enter the action you would like to do by entering the coressponding letter." << endl;
     cout << "ADD = 0" << endl;
     cout << "PRINT = 1" << endl;
