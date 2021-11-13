73fc8d307c204ea73ac22e77123e567fa075bb09
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-11-13 15:18:18 -0800

    Update slist.cpp

--- slist.cpp
+++ slist.cpp
@@ -19,89 +19,37 @@ int main(){
  jake.sID=8;
  cout<<"ID: "<<(jake.sID)<<endl;   
  students.push_back(jake);
- for(vector<student>::iterator it = students.begin(); it != students.end(); it++){
  
-  cout << it->sID << endl;
- }
-  
-}
-
-/*void printl(char* c);
-//student jake;
-//student *ptr_student;
-//ptr_student = &jake;
-void add(vector<student> &vect){
-  student new;
-  new.sID = 123;
-  vect.push_back(new);
-  for(auto & elem : students)
-    {
-        cout<<elem<<", ";
-    }
-}
-int main(){
-  
-  struct student{
-    
-    //char fname[100];
-    //char lname[100];
-    int sID;
-    //float gpa;
-  };
-  
-  vector<student> students;
-  
-  char name[50];
-  cin >> name;
-  
-  printl(name);
-  cout << "Welcome to the Student List." << endl;
-  cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
-  cout << "ADD -> Creates a new entry for student" << endl;
-  cout << "PRINT -> Prints the current students stored" << endl;
-  cout << "DELETE -> Deletes entry based on student ID" << endl;
-  cout << "QUIT -> Exits the program" << endl;
-  int loop = 0;
-  while(loop == 0){
-    cout << "What would you like to do?" << endl;
-    cout << "Enter the action you would like to do by entering the coressponding letter." << endl;
-    cout << "ADD = 0" << endl;
-    cout << "PRINT = 1" << endl;
-    cout << "DELETE = 2" << endl;
-    cout << "QUIT = 3" << endl;
-    int choice;
-    cin >> choice;
-    if(choice == 0){
-       add(student);
-    }
-    if(choice == 1){
-      
-    }
-    if(choice == 2){
-      
-    }
-    if(choice == 3){
-      loop = 1;
-      break;
-    }
+ cout << "Welcome to the Student List." << endl;
+ cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
+ cout << "ADD -> Creates a new entry for student" << endl;
+ cout << "PRINT -> Prints the current students stored" << endl;
+ cout << "DELETE -> Deletes entry based on student ID" << endl;
+ cout << "QUIT -> Exits the program" << endl;
+ int loop = 0;
+ while(loop == 0){
+  cout << "What would you like to do?" << endl;
+  cout << "Enter the action you would like to do by entering the coressponding letter." << endl;
+  cout << "ADD = 0" << endl;
+  cout << "PRINT = 1" << endl;
+  cout << "DELETE = 2" << endl;
+  cout << "QUIT = 3" << endl;
+  int choice;
+  cin >> choice;
+  if(choice == 0){
   }
+  if(choice == 1){  
+     for(vector<student>::iterator it = students.begin(); it != students.end(); it++){
+         cout << it->sID << endl;
+     }
+  }
+  if(choice == 2){  
+  }
+  if(choice == 3){
+    loop = 1;
+    break;
+  }
+  }
+  
 }
-void printl(char* c){
-    
-      int i = 0;
-      c[i] = 'a';
-      cout << c[i];
-      i++;
-      c[i] = 'b';
-       cout << c[i];
-      i++;
-      c[i] = 'c';
-       cout << c[i];
-      i++;
-      c[i] = 'd';
-      cout << c[i];
-      i++;
-      c[i] = 'e';
-      cout << c[i];
-      i++;
-}*/
+  
