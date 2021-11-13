e6d8b5d1cd17f4bf0653f7f9611ecb9322410f9f
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-11-12 20:55:02 -0800

    Update slist.cpp

--- slist.cpp
+++ slist.cpp
@@ -5,29 +5,51 @@
 
 using namespace std;
 
+struct student{
+
+  char fname[100];
+  char lname[100];
+  int sID;
+  float gpa;
+
+};
+
 int main(){
 
-  //cout << "Welcome to the Student List." << endl;
-  cout << "What would you like to do?" << endl;
-  cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
-  cout << "ADD -> Creates a new entry for student" << endl;
-  cout << "PRINT -> Prints the current students stored" << endl;
-  cout << "DELETE -> Deletes entry based on student ID" << endl;
-  cout << "QUIT -> Exits the program" << endl;
-  cout << "Enter the action you would like to do by entering the coressponding letter." << endl;
-  cout << "ADD = 0" << endl;
-  cout << "PRINT = 1" << endl;
-  cout << "DELETE = 2" << endl;
-  cout << "QUIT = 3" << endl;
-    
+  cout << "Welcome to the Student List." << endl;
+
+  while(0){
+
+    cout << "What would you like to do?" << endl;
+    cout << "You can ADD, PRINT, DELETE, or QUIT" << endl;
+    cout << "ADD -> Creates a new entry for student" << endl;
+    cout << "PRINT -> Prints the current students stored" << endl;
+    cout << "DELETE -> Deletes entry based on student ID" << endl;
+    cout << "QUIT -> Exits the program" << endl;
+    cout << "Enter the action you would like to do by entering the coressponding letter." << endl;
+    cout << "ADD = 0" << endl;
+    cout << "PRINT = 1" << endl;
+    cout << "DELETE = 2" << endl;
+    cout << "QUIT = 3" << endl;
+
+    int choice;
+    cin >> choice;
+
+    if(choice == 0){
+      cout << choice << endl;
+    }
+    if(choice == 1){
+      cout << choice << endl;
+    }
+    if(choice == 2){
+      cout << choice << endl;
+    }
+    if(choice == 3){
+      break;
+    }
+
+  }
+
 }
-
-
-
-
-
-
-
-
 
 
